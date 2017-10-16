dd56b5bb073e0aebded7c8f0dd0e8f30966725b3
sunmaer <878131732@qq.com>
2017-10-16 21:37:19 +0800

    链串简单模式匹配

--- Alogorithms/链串简单模式匹配.cpp
+++ Alogorithms/链串简单模式匹配.cpp
@@ -11,7 +11,7 @@ typedef struct LKString { // 链串存储结构
 } LKString, *LKStringList;
 
 void Create_LKString(LKStringList &L, string s) { // 创建链串
-  LKString *p,*r;
+  LKString *p, *r;
   L = (LKStringList)malloc(sizeof(LKString));
   r = L; // 尾结点
   for(int i=0; i<s.length(); i++) {
@@ -33,14 +33,34 @@ void Display_LKString(LKStringList L) { // 链串的显示
 
 LKString * StrIndex(LKStringList &s, LKStringList &t) {
   /* 求模式串 t 在主串 s 中第一次出现的位置指针 */
-  
+  LKString *sp, *tp, *start;
+
+  if(!t->next) return s->next; // 空串是任意串的子串
+  start = s-> next; // 记录主串的起始比较位置
+  sp = start; // 主串从 start 开始
+  tp= t->next; // 模式串从第一个结点开始
+
+  while(sp!=NULL && tp!=NULL) {
+    if(sp->ch == tp->ch) { // 若当前对应字符相同，则继续比较
+      sp = sp->next;
+      tp = tp->next;
+    } else {
+      start = start->next;
+      sp = start;
+      tp = t->next;
+    }
+  }
+
+  if(tp == NULL) return start;
+  else return NULL;
 }
 
 int main() {
-  LKStringList s,t;
-  string s1 = "hello world!",s2 = "hello";
+  LKStringList s, t, pos;
+  string s1 = "hello world!", s2 = "hello";
   Create_LKString(s, s1); // 主串
   Create_LKString(t, s2); // 模式串
-  Display_LKString(t);
+  pos = StrIndex(s, t);
+  cout<<pos;
   return 0;
 }
