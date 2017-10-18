32b21a0995ac24b35973dde538c57edd759db19e
sunmaer <878131732@qq.com>
2017-10-18 20:05:24 +0800

    二维数组存储稀疏数组

---
+++ Alogorithms/稀疏矩阵-二维数组表示.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+/**
+ * 1) 矩阵转置的经典算法
+ * 采用矩阵的常规存储方式（二维数组）实现矩阵转置
+ */
+void transMatrix(int source[][7], int dest[][6]) {
+  for(int i=0; i<6; i++) {
+    for(int j=0; j<7; j++) {
+      dest[j][i] = source[i][j];
+    }
+  }
+}
+
+int main() {
+  int source[6][7] = {0, 12, 9, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0,  -3, 0, 0, 0, 0, 14, 0,  0, 0, 24, 0, 0, 0, 0,  0, 18, 0, 0, 0, 0, 0,  15, 0, 0, -7, 0, 0, 0};
+  int dest[7][6];
+  
+  cout<<"原矩阵："<<endl;
+  for(int i=0; i<6; i++) { 
+    for(int j=0; j<7; j++) {
+      printf("%3d  ", source[i][j]);
+    }
+    cout<<endl;
+  }
+
+  transMatrix(source, dest);
+  
+  cout<<endl<<"转置后的矩阵："<<endl;
+  for(int i=0; i<7; i++) {
+    for(int j=0; j<6; j++) {
+      printf("%3d  ", dest[i][j]);
+    }
+    cout<<endl;
+  }
+
+  return 0;
+}
