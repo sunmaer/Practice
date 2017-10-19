7c2456553fd845d0b5dbc57131b2e4e248dd71fc
sunmaer <878131732@qq.com>
2017-10-19 17:15:43 +0800

    列序递增转置法

--- Algorithms/稀疏矩阵-三元组表表示.cpp
+++ Algorithms/稀疏矩阵-三元组表表示.cpp
@@ -15,32 +15,67 @@ typedef struct {
 
 /**
  * 1) 列序递增转置法
+ * 采用按照被转置后矩阵三元组表A的列序递增的顺序进行转置，并依次送入转置后矩阵的三元组表B中，这样
+ * 一来，转置后矩阵的三元组表B恰好是以“行序为主序”的
  */
-void transMatrix(int source[][7], int dest[][6]) {
-  for(int i=0; i<6; i++) {
-    for(int j=0; j<7; j++) {
-      dest[j][i] = source[i][j];
+void transMatrix(TSMatrix source, TSMatrix *dest) {
+  dest->m = source.n;
+  dest->n = source.m;
+  dest->len = source.len;
+  
+  int k=0;
+  if(dest->len > 0) {
+    for(int i=0; i<source.n; i++) {
+      for(int j=0; j<source.len; j++) {
+        if(source.data[j].col == i) {
+          dest->data[k].row = source.data[j].col;
+          dest->data[k].col = source.data[j].row;
+          dest->data[k].e = source.data[j].e;
+          k++;
+        }
+      }
     }
   }
 }
 
 void display(TSMatrix m) { // 打印稀疏矩阵
-  
+  for(int i=0; i<m.m; i++) {
+    for(int j=0; j<m.n; j++) {
+      int flag = 0;
+      for(int k=0; k<m.len; k++) {
+        if((i+1) == m.data[k].row && (j+1) == m.data[k].col) {
+          flag = 1;
+          printf("%3d  ", m.data[k].e);
+        }
+      }
+      if(!flag) {
+        printf("%3d  ", 0);
+      }
+    }
+    cout<<endl;
+  }
 }
 
 int main() {
   TSMatrix source, dest;
-  source.data[0] = { 1, 2, 12 };
-  source.data[1] = { 1, 3, 9 };
-  source.data[2] = { 3, 1, -3 };
-  source.data[3] = { 3, 6, 14 };
-  source.data[4] = { 4, 3, 24 };
-  source.data[5] = { 5, 2, 18 };
-  source.data[6] = { 6, 1, 15 };
-  source.data[7] = { 6, 4, -7 };
+  source.data[0] = (Triple){ 1, 2, 12 };
+  source.data[1] = (Triple){ 1, 3, 9 };
+  source.data[2] = (Triple){ 3, 1, -3 };
+  source.data[3] = (Triple){ 3, 6, 14 };
+  source.data[4] = (Triple){ 4, 3, 24 };
+  source.data[5] = (Triple){ 5, 2, 18 };
+  source.data[6] = (Triple){ 6, 1, 15 };
+  source.data[7] = (Triple){ 6, 4, -7 };
   source.m = 6;
   source.n = 7;
   source.len = 8;
+
+  cout<<"原矩阵："<<endl;
+  display(source);
+
+  transMatrix(source, &dest);
+  cout<<endl<<"转置后矩阵："<<endl;
+  display(dest);
 
   return 0;
 }
