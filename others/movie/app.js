c943f44c5b655f3172bc7b5df7d907d1131d1cbb
sunmaer <878131732@qq.com>
2017-11-25 18:39:22 +0800

    模板编写

--- others/movie/app.js
+++ others/movie/app.js
@@ -1,9 +1,37 @@
 var express = require('express')
+var port = process.env.PORT || 3000
 var app = express()
 
+app.set('views', './views')
 app.set('view engine', 'jade')
-app.set('port', 3000)
+app.listen(port)
 
+console.log('movie started on port' + port)
+
+// index.page
 app.get('/', function(req, res) {
-  res.render('index', {title: 'imooc'})
+  res.render('index', {
+    title: 'movie 首页'
+  })
+})
+
+// detail.page
+app.get('/movie/:id', function(req, res) {
+  res.render('detail', {
+    title: 'movie 详情页'
+  })
+})
+
+// admin.page
+app.get('/admin/movie', function(req, res) {
+  res.render('admin', {
+    title: 'movie 后台录入页'
+  })
+})
+
+// list.page
+app.get('/admin/list', function(req, res) {
+  res.render('list', {
+    title: 'movie 列表页'
+  })
 })
